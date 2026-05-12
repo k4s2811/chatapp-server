dad2e50aed49625bf4c46ee710c4aeb0ef301e8d
Kunal Soni <kunalson836@gmail.com>
2026-05-12 08:01:06 +0530

    Enable TLS for Redis client connection

--- Chatservice/config/redis.js
+++ Chatservice/config/redis.js
@@ -9,6 +9,7 @@ export const setupRedis = async (io) => {
     pubClient = createClient({
         url: ENV_VARS.REDIS_URL,
         socket: {
+            tls = true,
             reconnectStrategy: (retries) => {
                 console.log(`[Redis] Reconnecting... Attempt ${retries}`);
                 const delay = Math.min(retries * 50, 2000);
